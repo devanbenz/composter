9880eeb0db6090e3067b1ad311785774c29f22ed
Devan <devandbenz@gmail.com>
2025-01-10 08:11:41 -0600

    feat: working on it

--- src/clock_replacer.rs
+++ src/clock_replacer.rs
@@ -1,6 +1,8 @@
-/// [Replacer] implements the page replacement policy. This is a custom
-/// built replacer algorithm that takes inspiration from second chance
-/// algorithms. It uses a bitmask to check for eviction.  
+/// [Replacer] implements the page replacement policy. This is a custom-built
+/// replacer algorithm that takes inspiration from second chance
+/// algorithms. It uses a bitmask to check for eviction.
+///
+/// Given a replacer size of N
 struct Replacer<T> {
     size: usize,
     curr_size: usize,
@@ -26,11 +28,22 @@ impl<T> Replacer<T> {
 
     pub fn current_size(&self) -> usize { self.size }
 
+    pub fn record_access(&mut self, node: T) {}
+
     pub fn evict(&mut self) {}
 
     pub fn set_evictable(&mut self) {}
+
+    fn first_unset_bit() {}
 }
 
 
 mod tests {
+    use crate::clock_replacer::Replacer;
+
+    #[test]
+    fn test_replacer_simple() {
+        let mut p = Replacer::<u8>::new(5);
+
+    }
 }
